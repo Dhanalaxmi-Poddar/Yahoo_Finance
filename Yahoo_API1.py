542d6dec9ab3773d4429bd2d16eb5ac8df7565a9
Dhanalaxmi-Poddar <93055311+Dhanalaxmi-Poddar@users.noreply.github.com>
2021-10-25 21:12:40 +0530

    Add files via upload

--- Yahoo_API1.py
+++ Yahoo_API1.py
@@ -1,14 +1,18 @@
 import json
 import requests
 import pprint
+import urllib3
 
 url='https://yfapi.net/v6/finance/quote'
-headers={'x-api-key':"dmdtbB7cBwYDb2EhFykra5YzdCGdyAAa6BO1xIL6"}
+headers={'x-api-key':"dmdtbB7cBwYDb2EhFykra5YzdCGdyAAa6BO1xIL6",
+         "User-Agent":"Defined"}
 querystring={"symbols":input("Stock Symbol:")}
 
 data=requests.request("GET",url,headers=headers,params=querystring)
 
 data_read=((data.json().get('quoteResponse').get('result')))
 
+#pprint.pprint(data.json().get('quoteResponse').get('result'))
+
 for data_name in data_read:
-    print('The previous market close price is',data_name['regularMarketPreviousClose'])
+    print('The previousMarketclosePrice is',data_name['regularMarketPreviousClose'])
